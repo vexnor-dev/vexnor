1ee1c14f8057f056ef7343f034ac6ba2b4b7d47a
Adrian Topala <adrian.topala@gmail.com>
2025-09-14 14:00:56 +0200

    WIP: updating "valnor" local tests to use actual generated db

---
+++ packages/valnor/src/lib/__tests__/codegen/pg/index.ts
@@ -0,0 +1,2 @@
+export * as one_sql from "./one_sql.schema.js";
+export * from "./postgres-library.js";
--- packages/valnor/vite.config.ts
+++ packages/valnor/vite.config.ts
@@ -1,5 +1,11 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
    plugins: [],
+   resolve: {
+      alias: {
+         valnor: resolve(__dirname, "./src/lib/index.ts"),
+      },
+   },
 });
