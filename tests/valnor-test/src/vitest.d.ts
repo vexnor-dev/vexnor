fb2aff70e50d120ccf5a8f324de84d39879a21d8
Adrian Topala <adrian.topala@gmail.com>
2025-11-15 12:33:20 +0100

    deleting valnor-test

--- tests/valnor-test/src/vitest.d.ts
+++
@@ -1,8 +0,0 @@
-// interface CustomMatchers {
-//    toEqualQuery(expected: string, message?: string): void;
-// }
-//
-// declare module "vitest" {
-//    interface Assertion<T = any> extends CustomMatchers {}
-//    interface AsymmetricMatchersContaining extends CustomMatchers {}
-// }
