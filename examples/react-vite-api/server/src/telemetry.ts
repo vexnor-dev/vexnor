b1e6a3cb808986f59e31a149f7aec3347d20df35
Adrian Topala <adrian.topala@gmail.com>
2026-06-12 11:11:43 +0200

    chore: remove legacy vite-node dep (fixes esbuild + vite moderate vulns)

--- examples/react-vite-api/server/src/telemetry.ts
+++ examples/react-vite-api/server/src/telemetry.ts
@@ -1,10 +1,10 @@
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-node";
-import { Resource } from "@opentelemetry/resources";
+import { resourceFromAttributes } from "@opentelemetry/resources";
 import { ATTR_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
 
 const sdk = new NodeSDK({
-   resource: new Resource({
+   resource: resourceFromAttributes({
       [ATTR_SERVICE_NAME]: "vexnor-react-vite-api",
    }),
    traceExporter: new ConsoleSpanExporter(),
